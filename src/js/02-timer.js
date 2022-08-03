0abda623220c55b8be1da3889c10d255c562ba5f
Andrii-Smolianinov <andr380v@gmail.com>
2022-08-03 15:09:21 +0300

    add task-3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,16 @@
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
 
 const daysEl = document.querySelector('[data-days]');
 const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
-const startButtonEl = document.querySelector('[data-start]');
-const selectorEl = document.querySelector('#datetime-picker')
+const startButtonEl = document.querySelector('button');
+const selectorEl = document.querySelector('#datetime-picker');
 const currentData = new Date();
 let futureData = 0;
+startButtonEl.disabled = true;
 
 const options = {
   enableTime: true,
@@ -22,26 +23,25 @@ flatpickr(selectorEl, options);
 
 function onClose(selectedDates) {
   if (currentData > selectedDates[0]) {
-    Notiflix.Notify.failure('Будь-ласка, виберіть дату з майбутнього');
+    Notiflix.Notify.failure('❌Будь-ласка, виберіть дату з майбутнього');
   }
   if (currentData < selectedDates[0]) {
+    Notiflix.Notify.success('✅Супер! Стартуєм!!!');
+    startButtonEl.disabled = false;
     futureData = selectedDates[0];
-    removeDisable();
   }
-}
-function removeDisable() {
-  startButtonEl.removeAttribute('disabled');
 }
 
 startButtonEl.addEventListener('click', () => {
   setInterval(timer, 1000);
+  startButtonEl.disabled = true;
+  selectorEl.disabled = true;
 });
 
-function timer() {
+function timer() {  
   let today = new Date();
   const differenceData = futureData - today;
   const formatingMilSec = differenceData / 1000;
-
   const seconds = Math.floor(formatingMilSec) % 60;
   const minutes = Math.floor(formatingMilSec / 60) % 60;
   const hours = Math.floor(formatingMilSec / 60 / 60) % 24;
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,33 +1,30 @@
-// import Notiflix from 'notiflix';
-// const refs = {
-//   form: document.querySelector('.form'),
-//   delay: document.querySelector('input[name=delay]'),
-//   step: document.querySelector('input[name=step]'),
-//   amount: document.querySelector('input[name=amount]'),
-// };
+import Notiflix from 'notiflix';
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('input[name=delay]');
+const stepEl = document.querySelector('input[name=step]');
+const amountEl = document.querySelector('input[name=amount]');
+formEl.addEventListener('submit', onSubmitForm);
 
-// refs.form.addEventListener('submit', onSubmitForm);
-
-// function onSubmitForm(e) {
-//   e.preventDefault();
-//   const amount = refs.amount.value;
-//   const delay = refs.delay.value;
-//   const step = refs.step.value;
-//   for (let i = 1; i <= amount; i += 1) {
-//     setTimeout(() => {
-//       const position = i;
-//       const newDelay = (i - 1) * step + +delay;
-//       createPromise(position, newDelay)
-//         .then(value => {
-//           Notiflix.Notify.success(`${value}`);
-//         })
-//         .catch(error => {
-//           Notiflix.Notify.failure(`${error}`);
-//         });
-//     }, i * step);
-//   }
-//   e.target.reset();
-// }
+function onSubmitForm(e) {
+  e.preventDefault();
+  const amount = amountEl.value;
+  const delay = delayEl.value;
+  const step = stepEl.value;
+  for (let i = 1; i <= amount; i += 1) {
+    setTimeout(() => {
+      const position = i;
+      const newDelay = (i - 1) * step + +delay;
+      createPromise(position, newDelay)
+        .then(value => {
+          Notiflix.Notify.success(`${value}`);
+        })
+        .catch(error => {
+          Notiflix.Notify.failure(`${error}`);
+        });
+    }, i * step);
+  }
+  e.target.reset();
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
